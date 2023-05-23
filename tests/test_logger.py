26707a48a351f4195a11978b3c3d445b917c10b7
Jeremy Gibson <jgibso3@ncsu.edu>
2023-05-23 15:00:11 -0400

    Add tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,9 +1,11 @@
 import logging
 import os
+from unittest import mock
 
 TMP_FILE = "/tmp/test.log"
 
 
+@mock.patch.dict(os.environ, {"USELESS": "VALUE"}, clear=True)
 def test_default_logger(caplog):
     """Test the default logger"""
     from sat.logs import SATLogger
@@ -13,6 +15,16 @@ def test_default_logger(caplog):
     assert "Test message" in caplog.text
     logger.debug("DEBUG message")
     assert "DEBUG message" not in caplog.text
+
+
+@mock.patch.dict(os.environ, {"DEBUG": "1"}, clear=True)
+def test_debug_logger(caplog):
+    """Test the debug logger"""
+    from sat.logs import SATLogger
+
+    logger = SATLogger()
+    logger.debug("DEBUG message")
+    assert "DEBUG message" in caplog.text
 
 
 def test_add_handlers(caplog):
