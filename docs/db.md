a88a05c35c04cd20abbcce861b07cb53319e5d22
Shawn Taylor <staylor8@ncsu.edu>
2023-08-24 10:25:44 -0400

    Requested items complete. Add documentation.

---
+++ docs/db.md
@@ -0,0 +1,35 @@
+# Database Module
+
+The dabase module aims to standardize how we connect to our data environments
+
+It currently supports two database engines, Oracle and SQL server. 
+Support for SQL server is provided by pyodbc while support for Oracle is provided by cx_Oracle.
+
+There are two methods implemented by the module. The first method takes a 
+connection string paramater and an engine paramater and connects to the
+appropriate db engine.
+
+The second is a helper method that takes an envornment variable name 
+and retrieves the connection string from the environment and calls the primary.
+
+__install__
+
+`pip install --extra-index https://pypi.ehps.ncsu.edu sat-utils`
+
+__getting connected with a connection string__
+
+```
+from sat.db import get_db_connection
+from os import getenv
+
+connection_string = 'user/password@localhost/orcl'
+connection = get_db_connection(connection_string, 'oracle')
+```
+
+__getting connected with an environment variable__
+
+```
+from sat.db import get_named_db_connection
+
+connection = get_db_connection('ENV_PROD_CONNECTION', 'oracle')
+```
---
+++ sat/db.py
@@ -0,0 +1,78 @@
+from enum import Enum
+from os import getenv
+from sat.logs import SATLogger
+
+import cx_Oracle
+import pyodbc
+
+logger = SATLogger(name=__name__)
+
+class ConnectionType(Enum):
+    ORACLE = 1
+    SQL = 2
+
+def _get_connection_string(conn_env_var):
+    """
+    A helper function that returns a database connection
+    string from the environemnt.
+
+    Parameters
+    ----------
+    conn_env_var: str
+        The environment variable that contains the database 
+        connection string you are trying to connect to.
+    """
+
+    try:
+        connection_string = getenv(conn_env_var)
+        return connection_string
+    except ValueError as error:
+        
+        message = f"""
+                    There was an error retrieveing the connection string.
+                    Does {conn_env_var} exist in the local environment?
+                    """
+        
+        logger.error(message)
+        logger.error(error)
+
+def get_db_connection(conn_string:str, type:ConnectionType):
+    """
+    A function that returns a database connection.
+
+    Parameters
+    ----------
+    conn_str: str
+        The database specific connection string you are opening 
+         a connection to.
+    type: str
+        The database drive the connection uses.
+        Currently only 'oracle' or 'sql' are supported options.
+    return: Returns a database connection object
+    """
+    try:
+        if type == ConnectionType.SQL: 
+            return pyodbc.connect(conn_string)
+        
+        return cx_Oracle.connect(conn_string)
+    except Exception as error:
+        message = f"There was an error connecting to the database\n {error}"
+        logger.error(message)
+
+def get_named_db_connection(db_env_name:str, type:ConnectionType):
+    """
+    A function that returns a database connection.
+
+    Parameters
+    ----------
+    db_env_name: str
+        The name of a defined environment variable that contains a 
+         specific connection string you are opening a connection to.
+    type: str
+        The database drive the connection uses.
+        Currently only 'oracle' or 'sql' are supported options.
+    return: Returns a database connection object 
+    """
+    _conn_string = _get_connection_string(db_env_name)
+    return get_db_connection(conn_string=_conn_string, type=type)
+    
