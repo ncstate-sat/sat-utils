73a5aa7091102d21191b88b5bbc97844d0f82b12
Jeremy Gibson <jmgibso3@ncsu.edu>
2023-09-13 11:45:53 -0400

    SU-2 Adds the new oracle connection option
    Updates test and error handlers

--- sat/db.py
+++ sat/db.py
@@ -1,40 +1,47 @@
 from enum import Enum
 
 import cx_Oracle
+import oracledb
 import pyodbc
 
 
 class SatDBException(BaseException):
-    def __init__(self, message, error):
-        super().__init__(message)
-        self.error = error
+    pass
 
 
 class ConnectionType(Enum):
-    ORACLE = 1
-    SQL = 2
+    CX_ORACLE = 1
+    PY_ORACLE = 2
+    SQL = 3
 
 
-def get_db_connection(conn_string: str, conn_type: ConnectionType):
+def get_db_connection(conn_type: ConnectionType, **kwargs):
     """
     A function that returns a database connection.
 
     Parameters
     ----------
-    conn_string: str
-        The database specific connection string you are opening
-         a connection to.
     conn_type: ConnectionType
         The database driver the connection uses.
+    kwargs: contains the connection string or connection dictionary.
+
     Returns
     -------
     A database connection object.
+
+    Raises
+    ------
+    SatDBException
     """
     try:
         if conn_type == ConnectionType.SQL:
-            return pyodbc.connect(conn_string)
-        return cx_Oracle.connect(conn_string)
+            return pyodbc.connect(kwargs.get("conn_string"))
+        if conn_type == ConnectionType.CX_ORACLE:
+            return cx_Oracle.connect(kwargs.get("conn_string"))
+        if conn_type == ConnectionType.PY_ORACLE:
+            params = kwargs.get("conn_dict")
+            return oracledb.connect(**params)
     except Exception as error:
         raise SatDBException(
-            f"There was an {error.__class__.__name__} when connecting to the database.", error
+            f"There was an {error.__class__.__name__} when connecting to the database: {error}"
         )
