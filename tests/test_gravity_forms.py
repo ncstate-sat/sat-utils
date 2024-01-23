6a1d96a157a8775b459f0bcc39b9a5f3ea5492d2
John Champion <johnt.champion@gmail.com>
2024-01-23 16:38:50 -0500

    remove test

--- tests/test_gravity_forms.py
+++ tests/test_gravity_forms.py
@@ -1,7 +1,6 @@
 from unittest.mock import patch
 
 import pytest
-from oauthlib.oauth1 import OAuth1Error
 from requests.exceptions import RequestException
 from sat.gravity_forms import GravityForms
 
@@ -22,14 +21,3 @@ def test_request_exception():
         with pytest.raises(RequestException) as ex:
             gravity_forms.get_sponsors()
     assert "Simulated request exception." in str(ex.value)
-
-
-def test_oauth_error():
-    with patch("requests_oauthlib.OAuth1Session") as mock_oauth_session:
-        gravity_forms = GravityForms(consumer_key="your_key", consumer_secret="your_secret")
-        mock_oauth_session.side_effect = OAuth1Error("Simulated OAuth error.")
-        with pytest.raises(Exception) as ex:
-            gravity_forms.get_sponsors()
-
-        assert isinstance(ex.value, OAuth1Error)
-        assert str(ex.value) == "Simulated OAuth error."
