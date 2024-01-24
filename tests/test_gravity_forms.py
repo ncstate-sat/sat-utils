bedc353add3d0c5837d45c4a25cec71629323546
John Champion <johnt.champion@gmail.com>
2024-01-24 09:15:31 -0500

    add test

--- tests/test_gravity_forms.py
+++ tests/test_gravity_forms.py
@@ -14,10 +14,19 @@ def test_environment_variable_error():
         assert True
 
 
-def test_request_exception():
+def test_request_exception_get_sponsors():
     with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
         gravity_forms = GravityForms(consumer_key="your_key", consumer_secret="your_secret")
         mock_get.side_effect = RequestException("Simulated request exception.")
         with pytest.raises(RequestException) as ex:
             gravity_forms.get_sponsors()
+    assert "Simulated request exception." in str(ex.value)
+
+
+def test_request_exception_get_cards_requested():
+    with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
+        gravity_forms = GravityForms(consumer_key="your_key", consumer_secret="your_secret")
+        mock_get.side_effect = RequestException("Simulated request exception.")
+        with pytest.raises(RequestException) as ex:
+            gravity_forms.get_cards_requested()
     assert "Simulated request exception." in str(ex.value)
