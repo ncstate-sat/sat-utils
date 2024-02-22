84721f13f726d9229dadff2612cd09fc7efc4bf0
John Champion <johnt.champion@gmail.com>
2024-02-22 14:50:49 -0500

    expand the gravity forms package

--- tests/test_gravity_forms.py
+++ tests/test_gravity_forms.py
@@ -7,18 +7,76 @@ from sat.gravity_forms import GravityForms
 def test_environment_variable_error():
     try:
         gravity_forms = GravityForms()
-        gravity_forms.get("/forms/2/entries")
+        gravity_forms.get(
+            "/forms/2/entries", params={"paging[current_page]": 1, "paging[page_size]": 5}
+        )
         assert False
     except ValueError:
         assert True
 
 
-def test_request_exception_get(caplog):
+def test_request_exception_get():
     with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
         mock_get.side_effect = RequestException("Simulated request exception.")
         gravity_forms = GravityForms(
             consumer_key="your_key", consumer_secret="your_secret", base_url="https://baseurl.edu"
         )
-        response = gravity_forms.get("/forms/2/entries")
-        assert response == {}
-        assert "Simulated request exception." in caplog.text
+        try:
+            _ = gravity_forms.get(
+                "/forms/2/entries", params={"paging[current_page]": 1, "paging[page_size]": 5}
+            )
+            assert False
+        except RequestException as e:
+            assert str(e) == "Simulated request exception."
+
+
+def test_get_forms_exception():
+    with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
+        mock_get.side_effect = RequestException("Simulated request exception.")
+        gravity_forms = GravityForms(
+            consumer_key="your_key", consumer_secret="your_secret", base_url="https://baseurl.edu"
+        )
+        try:
+            _ = gravity_forms.get_forms()
+            assert False
+        except RequestException as e:
+            assert str(e) == "Simulated request exception."
+
+
+def test_get_form_exception():
+    with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
+        mock_get.side_effect = RequestException("Simulated request exception.")
+        gravity_forms = GravityForms(
+            consumer_key="your_key", consumer_secret="your_secret", base_url="https://baseurl.edu"
+        )
+        try:
+            _ = gravity_forms.get_form("1234")
+            assert False
+        except RequestException as e:
+            assert str(e) == "Simulated request exception."
+
+
+def test_get_entries_exception():
+    with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
+        mock_get.side_effect = RequestException("Simulated request exception.")
+        gravity_forms = GravityForms(
+            consumer_key="your_key", consumer_secret="your_secret", base_url="https://baseurl.edu"
+        )
+        try:
+            _ = gravity_forms.get_entries()
+            assert False
+        except RequestException as e:
+            assert str(e) == "Simulated request exception."
+
+
+def test_get_entry_exception():
+    with patch("requests_oauthlib.OAuth1Session.get") as mock_get:
+        mock_get.side_effect = RequestException("Simulated request exception.")
+        gravity_forms = GravityForms(
+            consumer_key="your_key", consumer_secret="your_secret", base_url="https://baseurl.edu"
+        )
+        try:
+            _ = gravity_forms.get_entry("1234")
+            assert False
+        except RequestException as e:
+            assert str(e) == "Simulated request exception."
