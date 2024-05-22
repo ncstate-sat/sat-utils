52e6bb0c2f46cc7d3a6441655fa159a3862f5113
Jeremy Gibson <jmgibso3@ncsu.edu>
2024-05-22 14:37:14 -0400

    LIBS-14 Adds types and tests

--- sat/models/ccure/access.py
+++ sat/models/ccure/access.py
@@ -1,10 +1,12 @@
 from pydantic import UUID4, BaseModel
 
+from sat.models.ccure.types import FILLED_STRING
+
 
 class Clearance(BaseModel):
     object_id: int
     guid: UUID4
-    name: str
+    name: FILLED_STRING
 
 
 class Credential(BaseModel):
--- sat/models/ccure/assets.py
+++ sat/models/ccure/assets.py
@@ -1,12 +1,10 @@
-from typing import Optional
-
 from pydantic import UUID4, BaseModel
 
-from sat.models.ccure.types import ASSET_TYPES
+from sat.models.ccure.types import ASSET_TYPES, FILLED_STRING
 
 
 class Asset(BaseModel):
     object_id: int
-    name: str
+    name: FILLED_STRING
     guid: UUID4
-    asset_type: Optional[ASSET_TYPES]
+    asset_type: ASSET_TYPES
