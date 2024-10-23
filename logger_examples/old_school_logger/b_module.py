ac5bcff528846bea709f98e5c9defc3fd7ceb758
Edward Brennan <ewbrenna@ncsu.edu>
2024-10-23 17:35:26 -0400

    Remove examples of unused implementations

--- logger_examples/old_school_logger/b_module.py
+++
@@ -1,7 +0,0 @@
-from sat.logs import SATLogger
-
-logger = SATLogger(__name__)
-
-def b_function():
-    logger.info('B message')
-    return
--- logger_examples/sat_logger_setup/module_2.py
+++
@@ -1,7 +0,0 @@
-from sat.logs import SATLoggerStripped
-
-LOGGER = SATLoggerStripped(__name__)
-
-def some_other_function(): 
-    LOGGER.info('Did some stuff over here')
-    return None
